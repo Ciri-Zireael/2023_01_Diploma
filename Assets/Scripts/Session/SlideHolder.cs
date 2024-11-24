132df30d8e4481b83c5b5f650008905dcd97c35b
Ciri-Zireael <ewajastrzebska21@gmail.com>
2024-11-24 22:03:23 +0100

    Add presentation presets
    Add 3 different presentation presets and modify prefab for Slide and script for SlideHolder
    Add the slides display to the monitor in the classroom

--- Assets/Scripts/Session/SlideHolder.cs
+++ Assets/Scripts/Session/SlideHolder.cs
@@ -1,15 +1,35 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class SlideHolder : MonoBehaviour
 {
 	[SerializeField] Sprite[] images;
+	[SerializeField] string folderPath = "Presentation1";
 	Image canvas;
 	int currentImageIndex;
 
 	void Awake()
 	{
 		canvas = GetComponent<Image>();
+		LoadSlides();
+	}
+	
+	void LoadSlides()
+	{
+		// Load images from the Resources folder
+		var loadedTextures = Resources.LoadAll<Texture2D>(folderPath);
+		images = loadedTextures.Select(tex => Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Vector2.one * 0.5f)).ToArray();
+
+		if (images.Length == 0)
+		{
+			Debug.LogWarning("No slides found in folder: " + folderPath);
+		}
+		else
+		{
+			canvas.sprite = images[0];	
+		}
+		
 	}
 
 	public void NextSlide()
